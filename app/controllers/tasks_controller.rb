b7449717e58b35b12121ed42aecaad6cbc669496
teppeihida <teppeihida@gmail.com>
2017-07-29 10:01:34 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -15,35 +16,41 @@ class TasksController < ApplicationController
     @task = Task.new(task_params)
 
     if @task.save
-      flash[:success] = 'Task が正常に投稿されました'
+      flash[:success] = '正常に投稿されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'Task が投稿されませんでした'
+      flash.now[:danger] = '投稿されませんでした'
       render :new
     end
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
-
-    if @task.update(message_params)
-      flash[:success] = 'Task は正常に更新されました'
+    if @task.update(task_params)
+      flash[:success] = '正常に更新されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'Task は更新されませんでした'
+      flash.now[:danger] = '更新されませんでした'
       render :edit
     end
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
 
-    flash[:success] = 'Task は正常に削除されました'
-    redirect_to messages_url
+    flash[:success] = '正常に削除されました'
+    redirect_to tasks_url
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:content)
   end
 end
